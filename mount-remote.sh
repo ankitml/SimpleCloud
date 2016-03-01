ba5e3217187bb901bbac892291213dc36f3c78b8
Francisco Silveira <franciscosilveira463@gmail.com>
2016-03-01 23:45:11 +0000

    Working version, can handle any filesystem event so far including the renaming problem (which was not explicitly fixed, but testing shows it works probably by deleting/recreating). Testing with college wifi showed the initial sync is heavily dependant on wireless signal quality.

--- mount-remote.sh
+++ mount-remote.sh
@@ -2,61 +2,81 @@
 REMOTE_USERNAME="francisco"
 LOCAL_ROOT="/home/francisco/"
 SYNC_ROOT="/home/francisco/.Simplecloud/"
-#REMOTE_ADDRESS="192.168.1.91"
 REMOTE_ADDRESS="95.95.67.48"
 REMOTE_CONTAINER="/mnt/things/"
-DIRECT_DIRECTORIES=("Music" "Pictures" "Torrents" "Videos")
+DIRECT_DIRECTORIES=("Music" "Pictures" "Torrents" "Videos" "Comics")
 SYNC_DIRECTORIES=("Documents")
-#SYNC_DIRECTORIES=("Torrents")
 RSYNC_OPTIONS="-rltuv --progress --exclude=\".Trash*\""
 SSHFS_OPTIONS="-o password_stdin -o allow_other -o reconnect -o ServerAliveInterval=60"
 EXCLUDE_FROM_SYNC="( *.goutputstream-* | *.swp | *\~ )"
 
-function mount-direct-dirs {
+function mount-dirs {
+	read -s -p "Insert $REMOTE_USERNAME's password: " REMOTE_PASSWORD
+	echo ""
+	
 	for directory in "${DIRECT_DIRECTORIES[@]}"
 	do
-		echo "Mounting $directory in /home/francisco/$directory"
-		echo $REMOTE_PASSWORD | sshfs "$REMOTE_USERNAME"@"$REMOTE_ADDRESS":"$REMOTE_CONTAINER""$directory" /home/francisco/"$directory" $SSHFS_OPTIONS
+		DIR_PATH="$LOCAL_ROOT$directory"
+		echo "Mounting $directory in $DIR_PATH"
+		if [ ! -d "$DIR_PATH" ]
+		then
+			mkdir "$DIR_PATH"
+		fi
+		echo $REMOTE_PASSWORD | sshfs "$REMOTE_USERNAME"@"$REMOTE_ADDRESS":"$REMOTE_CONTAINER""$directory" "$DIR_PATH" $SSHFS_OPTIONS
+		MOUNTSTATUS=${PIPESTATUS[1]}
+		if [[ MOUNTSTATUS -ne 0 ]]
+		then
+			REMOTE_PASSWORD="kek"
+			unmount
+			exit 1
+		fi
 	done
-}
-
-function mount-sync-dirs {
+	
 	for directory in "${SYNC_DIRECTORIES[@]}"
 	do
-		echo "Mounting $directory in /home/francisco/."$directory"-remote"
-		if [ ! -d "$SYNC_ROOT""$directory" ]
+		DIR_PATH="$SYNC_ROOT$directory"
+		echo "Mounting $directory in $DIR_PATH"
+		if [ ! -d "$DIR_PATH" ]
 		then
-			mkdir "$SYNC_ROOT""$directory"
+			mkdir "$DIR_PATH"
 		fi
-		echo $REMOTE_PASSWORD | sshfs "$REMOTE_USERNAME"@"$REMOTE_ADDRESS":"$REMOTE_CONTAINER""$directory" "$SYNC_ROOT""$directory" $SSHFS_OPTIONS
+		echo $REMOTE_PASSWORD | sshfs "$REMOTE_USERNAME"@"$REMOTE_ADDRESS":"$REMOTE_CONTAINER""$directory" "$DIR_PATH" $SSHFS_OPTIONS
 		MOUNTSTATUS=${PIPESTATUS[1]}
-		if [ "$MOUNTSTATUS" -ne 0 ]
+		if [[ MOUNTSTATUS -ne 0 ]]
 		then
-			echo "Unsuccessful mount"
-			#exit 1
+			REMOTE_PASSWORD="kek"
+			unmount
+			exit 1
 		fi
 	done
+	REMOTE_PASSWORD="kek"
+	echo "Finished mounting"
 }
 
+# Performs a two-way sync to make sure the server and
+# the client have the same state. Only after this should
+# the event syncs be called
+# Time taken is heavily dependant on network quality
+# Only AFTER this will any changes in sync directories
+# be noticed and treated
 function initial-sync {
-	:
-}
-
-function sync-sleep {
-	while [ "$MOUNTSTATUS" -eq 0 ]
+	for directory in ${SYNC_DIRECTORIES[@]}
 	do
-		for directory in "${SYNC_DIRECTORES[@]}"
-		do
-			#pull
-			rsync $RSYNC_OPTIONS /home/francisco/."$directory"-remote/ /home/francisco/"$directory"/
-			#push
-			rsync $RSYNC_OPTIONS --delete /home/francisco/"$directory"/ /home/francisco/."$directory"-remote/
-			echo "Sleeping 10"
-			sleep 10
-		done
+		SOURCE="$SYNC_ROOT$directory/"
+		DESTINATION=$(get-destination "$SOURCE")
+		echo "I'm about to rsync $RSYNC_OPTIONS $SOURCE $DESTINATION"
+		# Pull from server to local
+		rsync $RSYNC_OPTIONS "$SOURCE" "$DESTINATION"
+		# Push from local to server
+		rsync $RSYNC_OPTIONS "$DESTINATION" "$SOURCE"
 	done
+	echo -e "\nFinished initial sync\n"
 }
 
+# Watches all the sync directories and their server
+# counterparts. Every time a file is created, modified or
+# deleted, it executes the corresponding code (removal or
+# rsync)
 function sync-watch {
 	# Start a cycle that reads each filesystem notification
 	inotifywait -m -r -e create,delete,modify,attrib,move  --format "%T %e %w%f" --timefmt "%d/%m/%y %T" --exclude "$EXCLUDE_FROM_SYNC" $LOCAL_ROOT${SYNC_DIRECTORIES[*]} $SYNC_ROOT${SYNC_DIRECTORIES[*]} | while read NOTIFICATION
@@ -67,6 +87,8 @@ function sync-watch {
 		NOTIFICATION_LIST=($NOTIFICATION)
 		TIME=${NOTIFICATION_LIST[0]}" "${NOTIFICATION_LIST[1]}
 		EVENT=${NOTIFICATION_LIST[2]}
+		# Regex substitutions to obtain the complete path
+		# of the file that caused the notification
 		SOURCE=$(echo "$NOTIFICATION" | sed "s#\($EVENT\|$TIME\)##g" | sed "s#^[[:space:]]*##g")
 		if [[ -d $SOURCE ]]
 		then
@@ -80,7 +102,7 @@ function sync-watch {
 		MOVED_FROM* | DELETE*)
 			# Remove the file on the destination
 			echo "I would now do $ rm $DESTINATION &"
-			rm "$DESTINATION" &
+			rm -rf "$DESTINATION" &
 			;;
 		MOVED_TO* | CREATE* | ATTRIB* | MODIFY*)
 			# Sync the new file/dir from the source to the destination
@@ -100,6 +122,7 @@ function sync-watch {
 	done
 }
 
+# Unmounts all the remote directories
 function unmount {
 	for directory in "${DIRECT_DIRECTORIES[@]}"
 	do
@@ -113,6 +136,10 @@ function unmount {
 	done
 }
 
+# Swaps the segment of a file path between the
+# local root and the sync root
+# Ex: /home/user/Documents/test.txt => /home/user/.SimpleCloud/Documents/text.txt
+# Where /home/user/ is the local root and /home/user/.SimpleCloud is the sync root
 function get-destination {
 	SOURCE=$1
 	if [[ $SOURCE == $SYNC_ROOT* ]]
@@ -124,11 +151,8 @@ function get-destination {
 	fi
 }
 
-trap "unmount; exit" SIGHUP SIGINT SIGTERM SIGKILL
-read -s -p "Insert $REMOTE_USERNAME's password: " REMOTE_PASSWORD
-echo ""
-#mount-direct-dirs
-#mount-sync-dirs
+trap "REMOTE_PASSWORD="kek"; unmount; exit" SIGHUP SIGINT SIGTERM SIGKILL
+mount-dirs
 initial-sync
 sync-watch
 unmount
